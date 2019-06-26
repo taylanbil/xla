5e8a875c4c9e60e347d52606574cd83a2dbb9b84
Taylan Bilal <taylanbil@gmail.com>
2019-06-26 23:17:14 +0000

    bug fixes for successful training

--- test/test_train_fairseq.py
+++ test/test_train_fairseq.py
@@ -7,13 +7,13 @@ TODO(taylanbil): DO NOT SUBMIT without a detailed description of test_train_fair
 import argparse
 import sys
 import os
+import math
 
 pytorch_folder = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(sys.argv[0]))))
 xla_folder = os.path.join(pytorch_folder, 'xla')
 fairseq_folder = os.path.join(pytorch_folder, 'fairseq')
 sys.path.insert(0, xla_folder)
 sys.path.insert(0, fairseq_folder)
-#sys.path.insert(0, pytorch_folder)
 
 import torch
 
@@ -43,7 +43,7 @@ def collate_tokens_new(values, pad_idx, eos_idx=None, left_pad=False, move_eos_t
     size = PAD_TO_LENGTH
     # done correcting
     res = values[0].new(len(values), size).fill_(pad_idx)
- 
+
     def copy_tensor(src, dst):
         assert dst.numel() == src.numel()
         if move_eos_to_beginning:
@@ -62,6 +62,7 @@ data_utils.collate_tokens = collate_tokens_new
 
 from fairseq import options, tasks, checkpoint_utils
 from fairseq.trainer import Trainer
+from fairseq.data import iterators
 from fairseq.meters import StopwatchMeter
 
 
@@ -71,14 +72,10 @@ def parse_args():
   # e.g. input tensor shapes need to be controlled via
   #   max_sentences, required_batch_size_multiple
   parser = options.get_training_parser()
-  args = options.parse_args_and_arch(parser)
-  parser = argparse.ArgumentParser(add_help=False)
   parser.add_argument('--num_cores', type=int, default=8)
   parser.add_argument('--pad_to_length', type=int, default=64)
   parser.add_argument('--use_gpu', action='store_true')
-  FLAGS, _leftovers = parser.parse_known_args()
-  assert not set(args.__dict__.keys()).intersection(set(FLAGS.__dict__.keys()))
-  FLAGS.__dict__.update(args.__dict__)
+  FLAGS = options.parse_args_and_arch(parser)
   if not FLAGS.use_gpu:
     if FLAGS.fp16:
       print('suppressing "fp16"')
@@ -102,10 +99,7 @@ def parse_args():
   return FLAGS
 
 
-def main_tpu(args, init_distributed=False):
-  print("Args\n---------")
-  print(args)
-
+def prepare_task(args):
   # Setup task, e.g., translation, language modeling, etc.
   task = tasks.setup_task(args)
 
@@ -119,7 +113,8 @@ def main_tpu(args, init_distributed=False):
     lambda: task.build_model(args), device_ids=DEVICES, drop_last=True)
   criteria = {device: task.build_criterion(args)
               for device in model_parallel._device_ids}
-  models = dict(zip(model_parallel._device_ids, model_parallel._models))
+  models = {model_parallel._get_model_device(model): model
+          for model in model_parallel._models}
   model, criterion = model_parallel._models[0], list(criteria.values())[0]
   print(model)
   print('| model {}, criterion {}'.format(args.arch, criterion.__class__.__name__))
@@ -127,46 +122,55 @@ def main_tpu(args, init_distributed=False):
     sum(p.numel() for p in model.parameters()),
     sum(p.numel() for p in model.parameters() if p.requires_grad),
   ))
-  trainer = Trainer(args, task, model, criterion)
   del model, criterion
 
   # Build trainers
   trainers = {device: Trainer(args, task, models[device], criteria[device])
               for device in model_parallel._device_ids}
-  lr = min(trainer.get_lr() for trainer in trainers.values())
+  trainer = trainers[DEVICES[0]]
+  lr = trainer.get_lr()
+
+  # TODO(taylanbil): for now, this next line is only creating the iterator.
+  # validate its behavior with the case where a checkpoint actually exists.
 
   # Load the latest checkpoint if one is available and restore the
   # corresponding train iterator
   extra_state, epoch_itr = checkpoint_utils.load_checkpoint(args, trainer)
+  return task, trainers, model_parallel, epoch_itr, lr
 
-  # Train until the learning rate gets too small
-  train_meter = StopwatchMeter()
-  train_meter.start()
-  valid_losses = [None]
-  valid_subsets = args.valid_subset.split(',')
+
+def main_tpu(args):
 
   def train_loop_fn(model, loader, device, context):
-    trainer = trainers[device]
+    trainer = trainers[str(device)]
     tracker = xm.RateTracker()
     for i, samples in loader:
+      print('device {}, step {}: begin'.format(device, i))
       trainer.train_step(samples)
       xm.optimizer_step(trainer.optimizer)
+      print('device {}, step {}: end'.format(device, i))
 
   def valid_loop_fn(model, loader, device, context):
     raise
 
   def keep_training(lr, epoch_itr, trainers):
-    return False
+    # Train until the learning rate gets too small
     max_epoch = args.max_epoch or math.inf
     max_update = args.max_update or math.inf
-    lr = min(trainer.get_lr() for trainer in trainers)
-    n_updates = max(trainer.get_num_updates() for trainer in trainers)
+    lr = min(trainer.get_lr() for trainer in trainers.values())
+    n_updates = max(trainer.get_num_updates() for trainer in trainers.values())
     return (lr > FLAGS.min_lr) and (epoch_itr.epoch < max_epoch) and (n_updates < max_update)
 
-  import pdb
-  pdb.set_trace()
+  print("Args\n---------")
+  print(args)
+
+  task, trainers, model_parallel, epoch_itr, lr = prepare_task(args)
+
+  train_meter = StopwatchMeter()
+  train_meter.start()
+  valid_losses = [None]
+  valid_subsets = args.valid_subset.split(',')
   while keep_training(lr, epoch_itr, trainers):
-    # train for one epoch
     update_freq = args.update_freq[epoch_itr.epoch - 1] \
         if epoch_itr.epoch <= len(args.update_freq) else args.update_freq[-1]
 
@@ -177,6 +181,8 @@ def main_tpu(args, init_distributed=False):
     )
     train_loader = iterators.GroupedIterator(itr, update_freq)
     model_parallel(train_loop_fn, train_loader)
+    print("Rate: {}".format(tracker.rate()))
+    print(torch_xla._XLAC._xla_metrics_report())
 
     # if not args.disable_validation and epoch_itr.epoch % args.validate_interval == 0:
     #   # TODO(taylanbil): implement validate
@@ -202,14 +208,12 @@ def main_tpu(args, init_distributed=False):
 if __name__ == '__main__':
   # override certain args so that we use XLA parallelism instead of torch.
   FLAGS = parse_args()
-  DEVICES = xm.get_xla_supported_devices(max_devices=FLAGS.num_cores)
   if FLAGS.use_gpu:
     import train as fairseq_train
     fairseq_train.cli_main()
   else:
     BATCH_SIZE = FLAGS.max_sentences
+    DEVICES = xm.get_xla_supported_devices(max_devices=FLAGS.num_cores)
     PAD_TO_LENGTH = FLAGS.pad_to_length
     main_tpu(FLAGS)
 
-  #print("Rate: {}".format(tracker.rate()))
-  #print(torch_xla._XLAC._xla_metrics_report())
